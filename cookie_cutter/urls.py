90ad1ba7a4583d0fd59bc64c673d1c6386900ed7
agconti <agc11d@gmail.com>
2015-06-02 12:08:43 -0400

    feat(cookie_cutter/urls): added users app

--- cookie_cutter/urls.py
+++ cookie_cutter/urls.py
@@ -5,7 +5,10 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+import users
+
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api/v1/', include(users.urls)),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
---
+++ cookie_cutter/users/urls.py
@@ -0,0 +1,9 @@
+from __future__ import unicode_literals
+
+from django.conf.urls import url
+
+import views
+
+urlpatterns = [
+    url(r'^users/', views.UserView.as_view()),
+]
