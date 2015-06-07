ef3405f7381cd074a9443ba47c546b74d4eff05b
agconti <agc11d@gmail.com>
2015-06-06 20:07:19 -0400

    fix(requirements/base): updated whitenoise placement

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/common.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/common.py
@@ -189,25 +189,10 @@ class Common(Configuration):
         # The amount of time, in seconds, that references to created images
         # should be stored in the cache. Defaults to `2592000` (30 days)
         'cache_length': 2592000,
-        # The name of the cache you'd like `django-versatileimagefield` to use.
-        # Defaults to 'versatileimagefield_cache'. If no cache exists with the name
-        # provided, the 'default' cache will be used instead.
         'cache_name': 'versatileimagefield_cache',
-        # The save quality of modified JPEG images. More info here:
-        # http://pillow.readthedocs.org/en/latest/handbook/image-file-formats.html#jpeg
-        # Defaults to 70
         'jpeg_resize_quality': 70,
-        # The name of the top-level folder within storage classes to save all
-        # sized images. Defaults to '__sized__'
         'sized_directory_name': '__sized__',
-        # The name of the directory to save all filtered images within.
-        # Defaults to '__filtered__':
         'filtered_directory_name': '__filtered__',
-        # The name of the directory to save placeholder images within.
-        # Defaults to '__placeholder__':
         'placeholder_directory_name': '__placeholder__',
-        # Whether or not to create new images on-the-fly. Set this to `False` for
-        # speedy performance but don't forget to 'pre-warm' to ensure they're
-        # created and available at the appropriate URL.
         'create_images_on_demand': False
     }
