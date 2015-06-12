e0f2d6b63a09237f6961f8e942ee7cba564a51d5
agconti <agc11d@gmail.com>
2015-06-12 08:53:40 -0400

    feat(config/production): updated django storages config
    fix(config/production): moved boto import

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/production.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/production.py
@@ -40,6 +40,25 @@ class Production(Common):
         )),
     )
 
+    # Media files
+    # http://django-storages.readthedocs.org/en/latest/index.html
+    INSTALLED_APPS += ('storages',)
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+    AWS_ACCESS_KEY_ID = values.Value('DJANGO_AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY = values.Value('DJANGO_AWS_SECRET_ACCESS_KEY')
+    AWS_STORAGE_BUCKET_NAME = values.Value('DJANGO_AWS_STORAGE_BUCKET_NAME')
+    AWS_AUTO_CREATE_BUCKET = True
+    MEDIA_URL = 'https://s3.amazonaws.com/{}/'.format(AWS_STORAGE_BUCKET_NAME)
+    from boto.s3.connection import OrdinaryCallingFormat
+    AWS_S3_CALLING_FORMAT = OrdinaryCallingFormat()
+
+    # https://developers.google.com/web/fundamentals/performance/optimizing-content-efficiency/http-caching#cache-control
+    # Response can be cached by browser and any intermediary caches (i.e. it is "public") for up to 1 day
+    # 86400 = (60 seconds x 60 minutes x 24 hours)
+    AWS_HEADERS = {
+        'Cache-Control': 'max-age=86400, s-maxage=86400, must-revalidate',
+    }
+
     # Static files
     STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
