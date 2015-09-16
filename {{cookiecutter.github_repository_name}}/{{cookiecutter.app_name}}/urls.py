b59a0deb4a2c283a43318749ececc2f01759bcf6
agconti <agc11d@gmail.com>
2015-09-15 20:15:46 -0400

    chore(cookicutter/urls): updated api-root redirect view

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/urls.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
 
     # the 'api-root' from django rest-frameworks default router
     # http://www.django-rest-framework.org/api-guide/routers/#defaultrouter
-    url(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'))),
+    url(r'^$', RedirectView.as_view(url=reverse_lazy('api-root'), permanent=True)),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
