638de7b68d145939b2edd86f067ff55060849678
Pan Long <aga.panlong@gmail.com>
2016-04-30 16:38:14 +0800

    Delete a comment in user permissions
    We don't assume the model instance has an `owner` attribute, which is confusing.

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/permissions.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/permissions.py
@@ -4,7 +4,6 @@ from rest_framework import permissions
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
     Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
     """
 
     def has_object_permission(self, request, view, obj):
