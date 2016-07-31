72d9cd189709bfc5ac3491e20253769d8c4ba2d0
Andrew Conti <agc11d@gmail.com>
2016-07-31 13:59:07 -0400

    feat(config/common): allowed pagination to be configurable

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/common.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/common.py
@@ -167,7 +167,7 @@ class Common(Configuration):
     # Django Rest Framework
     REST_FRAMEWORK = {
         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-        'PAGE_SIZE': 100,
+        'PAGE_SIZE': int(os.getenv('DJANGO_PAGINATION_LIMIT', 10)),
         'DATETIME_FORMAT': '%Y-%m-%dT%H:%M:%S%z',
         'DEFAULT_RENDERER_CLASSES': (
             'rest_framework.renderers.JSONRenderer',
