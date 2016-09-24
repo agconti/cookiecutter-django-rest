9d2c7b24e98f52b15486ae6e800aed4b119f5c20
Andrew Conti <agc11d@gmail.com>
2016-09-23 21:54:41 -0400

    fix(user/admin): added user model import

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/admin.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
 from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import User
+
 
 @admin.register(User)
 class UserAdmin(AuthUserAdmin):
