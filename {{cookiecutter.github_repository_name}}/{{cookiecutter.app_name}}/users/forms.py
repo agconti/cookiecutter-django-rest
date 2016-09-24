82843acee13bdd9248e8c663af3ef13d6dc29fb3
Andrew Conti <agc11d@gmail.com>
2016-09-23 22:00:04 -0400

    feat(users/forms): seperated forms and admins (#251)
    * feat(users/forms): seperated forms and admins
    
    * fix(user/admin): added user model import

---
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/forms.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import, unicode_literals
+from django import forms
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from .models import User
+
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta(UserChangeForm.Meta):
+        model = User
+
+
+class CustomUserCreationForm(UserCreationForm):
+
+    # http://james.lin.net.nz/2013/06/08/django-custom-user-model-in-admin-relation-auth_user-does-not-exist/
+    def clean_username(self):
+        # Since User.username is unique, this check is redundant,
+        # but it sets a nicer error message than the ORM. See #13147.
+        username = self.cleaned_data["username"]
+        try:
+            User._default_manager.get(username=username)
+        except User.DoesNotExist:
+            return username
+        raise forms.ValidationError(self.error_messages['duplicate_username'])
+
+    class Meta(UserCreationForm.Meta):
+        model = User
