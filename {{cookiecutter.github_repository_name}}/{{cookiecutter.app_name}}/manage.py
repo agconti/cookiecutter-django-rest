211bbdc029ef91041927c84fd02395ffe6ea59ee
agconti <agc11d@gmail.com>
2016-12-19 13:01:00 -0500

    feat(manage): updated with django 1.10 boilerplate

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/manage.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/manage.py
@@ -6,6 +6,19 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config")
     os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
 
-    from configurations.management import execute_from_command_line
-
+    try:
+        from configurations.management import execute_from_command_line
+    except ImportError:
+        # The above import may fail for some other reason. Ensure that the
+        # issue is really that Django is missing to avoid masking other
+        # exceptions on Python 2.
+        try:
+            import django  # noqa
+        except ImportError:
+            raise ImportError(
+                "Couldn't import Django. Are you sure it's installed and "
+                "available on your PYTHONPATH environment variable? Did you "
+                "forget to activate a virtual environment?"
+            )
+        raise
     execute_from_command_line(sys.argv)
