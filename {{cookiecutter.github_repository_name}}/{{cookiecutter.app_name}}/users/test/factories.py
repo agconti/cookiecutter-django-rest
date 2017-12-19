6b67543944c27820cc0a45a5d9cc3602cfcac312
agconti <agc11d@gmail.com>
2017-12-19 17:08:38 -0500

    style(test/factories): use fstring

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/test/factories.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/test/factories.py
@@ -9,7 +9,7 @@ class UserFactory(factory.django.DjangoModelFactory):
         django_get_or_create = ('username',)
 
     id = factory.Sequence(lambda n: uuid.uuid4())
-    username = factory.Sequence(lambda n: 'testuser{}'.format(n))
+    username = factory.Sequence(lambda n: f'testuser{n}')
     password = factory.Faker('password', length=10, special_chars=True, digits=True, upper_case=True, lower_case=True)
     email = factory.Faker('email')
     first_name = factory.Faker('first_name')
