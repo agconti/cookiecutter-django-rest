76dfd7c828b0dbed6c149fbbd00242d7a31ef0a1
agconti <agc11d@gmail.com>
2017-12-19 17:05:08 -0500

    refactor(wsgi): remove django whitenoise integration

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/wsgi.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/wsgi.py
@@ -1,10 +1,8 @@
 """
-WSGI config for {{cookiecutter.app_name}} project.
-
+WSGI config for viral project.
 It exposes the WSGI callable as a module-level variable named ``application``.
-
 For more information on this file, see
-https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+https://docs.djangoproject.com/ko/1.11/howto/deployment/wsgi/gunicorn/
 """
 import os
 
@@ -12,7 +10,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config")
 os.environ.setdefault("DJANGO_CONFIGURATION", "Production")
 
 from configurations.wsgi import get_wsgi_application  # noqa
-from whitenoise.django import DjangoWhiteNoise        # noqa
-
 application = get_wsgi_application()
-application = DjangoWhiteNoise(application)
