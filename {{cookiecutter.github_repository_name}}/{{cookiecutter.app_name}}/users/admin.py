22353965d18892b3d6c4bd887deddcbbbcc6e180
agconti <agc11d@gmail.com>
2017-12-19 17:05:35 -0500

    refactor(users/views): upgrade to latest drf standard

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/admin.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin as AuthUserAdmin
-from .forms import CustomUserChangeForm, CustomUserCreationForm
+from django.contrib.auth.admin import UserAdmin
 from .models import User
 
 
 @admin.register(User)
-class UserAdmin(AuthUserAdmin):
-    form = CustomUserChangeForm
-    add_form = CustomUserCreationForm
+class UserAdmin(UserAdmin):
+    pass
--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/models.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/models.py
@@ -1,10 +1,11 @@
-from __future__ import unicode_literals
-
 import uuid
 from django.db import models
+from django.conf import settings
+from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser
 from django.utils.encoding import python_2_unicode_compatible
-# from django.utils.translation import ugettext_lazy as _
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
 
 
 @python_2_unicode_compatible
@@ -13,3 +14,9 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
