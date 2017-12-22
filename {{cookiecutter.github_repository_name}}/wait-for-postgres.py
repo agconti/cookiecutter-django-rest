9f981a0e7d7f39743867e1f6b17684460cf0c760
agconti <agc11d@gmail.com>
2017-12-22 12:37:50 -0500

    feat(wait-for-postgres): pull from env vars

--- {{cookiecutter.github_repository_name}}/wait-for-postgres.py
+++ {{cookiecutter.github_repository_name}}/wait-for-postgres.py
@@ -1,27 +1,35 @@
-def pg_ready(host, port, dbuser='postgres', dbname='postgres',
-             timeout=20, poll_freq=0.2):
-    """Wait until a postgres instance is ready to receive connections.
-    .. note::
-        This requires psycopg2 to be installed.
-    :type host: str
-    :type port: int
-    :type timeout: float
-    :type poll_freq: float
-    """
-    import psycopg2
-    t0 = time.time()
-    while time.time() - t0 < timeout:
+import os
+import logging
+from time import time, sleep
+import psycopg2
+check_timeout = os.getenv("POSTGRES_CHECK_TIMEOUT", 30)
+check_interval = os.getenv("POSTGRES_CHECK_INTERVAL", 1)
+config = {
+    "dbname": os.getenv("POSTGRES_DB", "postgres"),
+    "user": os.getenv("POSTGRES_USER", "postgres"),
+    "password": os.getenv("POSTGRES_PASSWORD", ""),
+    "host": os.getenv("DATABASE_URL", "postgres")
+}
+
+start_time = time()
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+logger.addHandler(logging.StreamHandler())
+
+
+def pg_isready(host, user, password, dbname):
+    while time() - start_time < check_timeout:
         try:
-            conn = psycopg2.connect(
-                "host={host} port={port} user={dbuser} "
-                "dbname={dbname}".format(**vars())
-            )
-            logger.debug('Connected successfully.')
+            conn = psycopg2.connect(**vars())
+            logger.info("Postgres is ready!")
             conn.close()
             return True
-        except psycopg2.OperationalError as ex:
-            logger.debug("Connection failed: {0}".format(ex));
-        time.sleep(poll_freq)
+        except psycopg2.OperationalError:
+            logger.info(f"Postgres is not ready. Waiting {check_interval} seconds...")
+            sleep(check_interval)
 
-    logger.error('Postgres readiness check timed out.')
+    logger.error("Postgres readiness check timed out.")
     return False
+
+
+pg_isready(**config)
