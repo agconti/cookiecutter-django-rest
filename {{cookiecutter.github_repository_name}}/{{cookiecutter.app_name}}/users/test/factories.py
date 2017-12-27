046aace1b08094e6314ddf86c91de9c98fd856d8
agconti <agc11d@gmail.com>
2017-12-27 14:08:43 -0500

    style(test/factories): fix line length violation

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/test/factories.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/test/factories.py
@@ -10,7 +10,8 @@ class UserFactory(factory.django.DjangoModelFactory):
 
     id = factory.Sequence(lambda n: uuid.uuid4())
     username = factory.Sequence(lambda n: f'testuser{n}')
-    password = factory.Faker('password', length=10, special_chars=True, digits=True, upper_case=True, lower_case=True)
+    password = factory.Faker('password', length=10, special_chars=True, digits=True,
+                             upper_case=True, lower_case=True)
     email = factory.Faker('email')
     first_name = factory.Faker('first_name')
     last_name = factory.Faker('last_name')
