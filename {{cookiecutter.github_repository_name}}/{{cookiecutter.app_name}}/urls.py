dcc7a5b3890870bbc7e7a8fdd341d9514ee75874
agconti <agc11d@gmail.com>
2017-12-27 21:44:52 -0500

    fix(urls): revert static import

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/urls.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/urls.py
@@ -1,6 +1,6 @@
 from django.conf import settings
 from django.urls import path, re_path, include, reverse_lazy
-from django.urls.static import static
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic.base import RedirectView
 from rest_framework.routers import DefaultRouter
