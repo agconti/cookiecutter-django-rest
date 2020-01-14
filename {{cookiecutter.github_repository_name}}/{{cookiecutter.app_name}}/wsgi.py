f770c67070021a655fe1b246c30c6c11db54589b
Andrew Conti <agc11d@gmail.com>
2020-01-14 11:00:56 -0500

    docs(wsgi): update docstring with project name

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/wsgi.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for viral project.
+WSGI config for {{ cookiecutter.app_name }} project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.0/howto/deployment/wsgi/gunicorn/
