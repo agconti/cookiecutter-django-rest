12a0738fd8ce00a6abefbe689cf902d0b20bff91
agconti <agc11d@gmail.com>
2020-01-14 12:53:45 -0500

    feat(users/models): drop python 2 support

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/models.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/users/models.py
@@ -3,12 +3,10 @@ from django.db import models
 from django.conf import settings
 from django.dispatch import receiver
 from django.contrib.auth.models import AbstractUser
-from django.utils.encoding import python_2_unicode_compatible
 from django.db.models.signals import post_save
 from rest_framework.authtoken.models import Token
 
 
-@python_2_unicode_compatible
 class User(AbstractUser):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
 
