ecfafd537c7a8755b3b5a54c80e0c650be575a1e
Andrew Conti <agc11d@gmail.com>
2024-08-30 13:50:35 -0400

    wip

--- {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/local.py
+++ {{cookiecutter.github_repository_name}}/{{cookiecutter.app_name}}/config/local.py
@@ -8,15 +8,6 @@ class Local(Common):
 
     # Testing
     INSTALLED_APPS = Common.INSTALLED_APPS
-    INSTALLED_APPS += ('django_nose',)
-    TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-    NOSE_ARGS = [
-        BASE_DIR,
-        '-s',
-        '--nologcapture',
-        '--with-coverage',
-        '--cover-package={{cookiecutter.app_name}}'
-    ]
 
     # Mail
     EMAIL_HOST = 'localhost'
